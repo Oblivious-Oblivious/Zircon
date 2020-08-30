c309165d013991f75583b4f93438d3da7bb1c51f
Oblivious-Oblivious <me262thanos@gmail.com>
2020-08-30 18:13:00 +0300

    Removed unused files and R&D notes.

--- examples/hash.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-join_char = "";
-puts join_char + ARGV[0]
-    .chomp.split(//)
-    .map(&:ord)
-    .map { |v| v.to_s(16) }
-    .join(join_char);
--- examples/helpers/stdbool.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STDBOOL_H_
-#define __STDBOOL_H_
-
-/** @param bool -> A 'big' enough size to hold both 1 and 0 **/
-typedef unsigned char bool;
-#define true 1
-#define false 0
-
-#endif
