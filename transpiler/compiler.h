f8231f443c060004b33f8744b137336ae369ef39
Oblivious-Oblivious <me262thanos@gmail.com>
2020-09-19 21:15:11 +0300

    Small changes in the filesystems.

--- transpiler/compiler.h
+++ transpiler/compiler.h
@@ -40,46 +40,44 @@ void add_includes_to_tranlation(string *inc) {
 void compile_file(char *file_to_compile) {
     int i;
     for(i = total_i_values; i < argc; i++) {
+        __setup_hashmaps();
+        /**/
+        hashmap_add(typedef_names, "size_t", (void*)true);
+        hashmap_add(typedef_names, "bool", (void*)true);
+        /**/
+        
+        /* printf("\033[38;5;206mCompiling: `%s`\033[0m\n", argv[i]); */
+        yyin = fopen(argv[i], "r");
+        /* printf("\033[38;5;206mCompiling: `%s`\033[0m\n", file_to_compile); */
+        /* yyin = fopen(file_to_compile, "r"); */
+        translation = new_string("");
 
-    __setup_hashmaps();
-    /**/
-    hashmap_add(typedef_names, "size_t", (void*)true);
-    hashmap_add(typedef_names, "bool", (void*)true);
-    /**/
-    
-    /* printf("\033[38;5;206mCompiling: `%s`\033[0m\n", argv[i]); */
-    yyin = fopen(argv[i], "r");
-    /* printf("\033[38;5;206mCompiling: `%s`\033[0m\n", file_to_compile); */
-    /* yyin = fopen(file_to_compile, "r"); */
-    translation = new_string("");
+        /* Parse the text */
+        yyparse();
 
-    /* Parse the text */
-    yyparse();
+        /* Write the init nodes */
+        if(main_flag) {
+            main_flag_was_set = true;
+            vector_map(include_list_for_main, (lambda)add_includes_to_tranlation);
+            __setup_init_objects();
+            filename = new_string("__zircon_main.c");
+            string_add_str(command, string_get(filename));
+        }
 
-    /* Write the init nodes */
-    if(main_flag) {
-        main_flag_was_set = true;
-        vector_map(include_list_for_main, (lambda)add_includes_to_tranlation);
-        __setup_init_objects();
-        filename = new_string("__zircon_main.c");
-        string_add_str(command, string_get(filename));
-    }
+        vector_add(files, filename);
 
-    vector_add(files, filename);
+        fp = fopen(string_get(filename), "w");
+        fprintf(fp, "%s", string_get(translation));
+        fclose(fp);
 
-    fp = fopen(string_get(filename), "w");
-    fprintf(fp, "%s", string_get(translation));
-    fclose(fp);
-
-    /* @@@ */
-    /* printf("\n\033[38;5;206mtypedef_names\033[0m\n");
-    display_hashmap(typedef_names);
-    printf("\n\033[38;5;206menum_constants\033[0m\n");
-    display_hashmap(enum_constants);
-    printf("\n\033[38;5;206mobject_names\033[0m\n");
-    display_hashmap(object_names); */
-    /* @@@ */
-
+        /* @@@ */
+        /* printf("\n\033[38;5;206mtypedef_names\033[0m\n");
+        display_hashmap(typedef_names);
+        printf("\n\033[38;5;206menum_constants\033[0m\n");
+        display_hashmap(enum_constants);
+        printf("\n\033[38;5;206mobject_names\033[0m\n");
+        display_hashmap(object_names); */
+        /* @@@ */
     }
 }
 
